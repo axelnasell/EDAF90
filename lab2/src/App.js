1812951558269ba97aa4ac1be0d9af6cc168ed10
JoelWaye <jo5502wa-s@student.lu.se>
2023-02-08 15:06:57 +0100

    hej

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -13,35 +13,49 @@ function App()
   setShoppingCart([...shoppingCart, salad]);
   };
 
+  function Header() { return (
+    <header className="pb-3 mb-4 border-bottom">
+      <span className="fs-4">Min egen salladsbar</span>
+    </header>
+  )}
+    
+   function NavBar(props) { return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item"></li>
+      {/* <NavLink className="nav-link" to="/compose-salad">
+      Komponera en sallad
+      </NavLink> */}
+    </ul>
+    );}
+    
+  function PageContent(props) { return (
+    <div className="container col-12">
+      <div className="row h-200 p-5 bg-light border rounded-3">
+        <h2>Bygg din sallad</h2>
+        <ComposeSalad inventory={inventory} onSubmit={handleSubmit} />
+        <ViewOrder components={shoppingCart}/>
+      </div>
+    </div>
+  )}
+
+  function Footer(props) { return(
+    <footer className="pt-3 mt-4 text-muted border-top">
+     EDAF90 - webprogrammering
+    </footer>
+  )  }
+
 
   // let extras = Object.keys(inventory).filter(name => inventory[name].extra);
 
   return (
     <div className="bg">
-    <div className="container py-4">
-    <header className="pb-3 mb-4 border-bottom">
-      <span className="fs-4">Min egen salladsbar</span>
-    </header>
-    <div className="container col-12">
-        <div className="row h-200 p-5 bg-light border rounded-3">
-          <h2>Bygg din sallad</h2>
-          <ComposeSalad inventory={inventory} onSubmit={handleSubmit} />
-          <ViewOrder components={shoppingCart}/>
-        </div>
+      <div className="container py-4">
+      <Header />
+      <NavBar /> 
+      <PageContent />
+      <Footer />
       </div>
     </div>
-
-    {/* <div className="continer col-12">
-      <div className="row h-200 p-5 bg-light border rounded-3">
-        <h2>Välj innehållet i din sallad</h2>
-          {extras.map(name => <div key={name} className="col-4">{name}</div>)}
-      </div>
-    </div> */}
-
-    <footer className="pt-3 mt-4 text-muted border-top">
-      EDAF90 - webprogrammering
-    </footer>
-  </div>
   );
   }
 
